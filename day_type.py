1669d63afb429830a41ff7bdbf63b7ba49c43689
Md Mominul Islam <64025349+mominulislam2329@users.noreply.github.com>
2020-05-18 11:55:58 -0500

    Input function and use of If-else statement

---
+++ day_type.py
@@ -0,0 +1,13 @@
+day_type=input('type of day: ' )
+if day_type=='hot':
+    print('Sunny day')
+    print('Drink plenty of water')
+#another if like cold is true then also print two messages
+elif day_type=='cold' :
+    print('its a cold day')
+    print('wear warm clothes')
+#neither hot or cold then print lovely day
+else:
+    print('lovely weather')
+#always print enjoy your day message
+print('Enjoy your day')
